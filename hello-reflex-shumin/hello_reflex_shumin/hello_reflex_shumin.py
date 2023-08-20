d16e1240e888a2223de330d27dcb654d72584e4e
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2023-08-20 16:29:25 +0800

    add box (#9)
    Great

--- hello-reflex-shumin/hello_reflex_shumin/hello_reflex_shumin.py
+++ hello-reflex-shumin/hello_reflex_shumin/hello_reflex_shumin.py
@@ -2,6 +2,7 @@
 from rxconfig import config
 
 import reflex as rx
+from . import style
 
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -12,6 +13,47 @@ class State(rx.State):
 
     pass
 
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
+        margin_y="1em",
+    )
+
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
+    )
+
+
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button("Ask", style=style.button_style),
+    )
 
 def index() -> rx.Component:
     return rx.fragment(
@@ -19,6 +61,8 @@ def index() -> rx.Component:
         rx.vstack(
             rx.heading("Welcome to Reflex!", font_size="2em"),
             rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
+            chat(),
+            action_bar(),
             rx.link(
                 "Check out our docs!",
                 href=docs_url,
