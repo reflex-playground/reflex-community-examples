680f9e39bec8b3d826938e913bf2630f3cd76465
Milo Chen <milochen0418@users.noreply.github.com>
2023-09-18 00:27:11 +0800

    fix: display new text with clean old text (#29)

--- iot-mqtt/wokwi-esp32/main.py
+++ iot-mqtt/wokwi-esp32/main.py
@@ -14,8 +14,22 @@ i2c = I2C(0, scl=Pin(22), sda=Pin(21))
 oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-oled.text('Hello, Wokwi!', 10, 10)      
-oled.show()
+
+
+def displayView(line1:str='', line2:str='', line3:str='', line4:str=''):
+  oled.fill(0)
+  oled.text(line1, 2, 2)
+  oled.text(line2, 2, 17)
+  oled.text(line3, 2, 32)
+  oled.text(line4, 2, 47)
+  oled.show()
+
+displayView(
+  'Hello, Urish!', 
+  'Hello, Milo!', 
+  'Hello, Alek!')
+
+
 
 
 buttonPin = Pin(18, Pin.IN) # D2 Pin: detect the button clicking
@@ -31,6 +45,11 @@ MQTT_PASSWORD  = ""
 MQTT_TOPIC     = "MiloTopic"
 
 print("Connecting to WiFi", end="")
+displayView(
+  'Connecting to', 
+  'Wifi ... ', 
+  '')
+
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect('Wokwi-GUEST', '')
@@ -38,13 +57,25 @@ while not sta_if.isconnected():
   print(".", end="")
   time.sleep(0.1)
 print(" Connected!")
-
+displayView(
+  'Wifi Connected', 
+  '', 
+  '')
 print("Connecting to MQTT server... ", end="")
+displayView(
+  'Wifi Connected.', 
+  'Connecting to ', 
+  'MQTT Server...')
+
 client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
 client.connect()
 
 print("Connected!")
-
+displayView(
+  'Wifi Connected.', 
+  'MQTT Connected. ', 
+  'Start to click ',
+  'button to count')
 while True:
   if buttonPin.value() == 1:
     client.publish(MQTT_TOPIC, "add")
